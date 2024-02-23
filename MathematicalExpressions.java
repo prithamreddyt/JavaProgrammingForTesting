a24522f59608c085b5a0071fae64b7065dd4dc69
prithamreddyt <prithamreddy100@gmail.com>
2024-02-23 13:14:38 +0530

    first commit

---
+++ MathematicalExpressions.java
@@ -0,0 +1,121 @@
+package Javasessions;
+
+public class MathematicalExpressions {
+
+	public static void main(String[] args) {
+		System.out.println("mathematical operations with integers");
+        System.out.println("");
+        int a=10;
+        int b=30;
+        
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a=b);
+        //division
+            System.out.println(a/b);
+        //for reminder
+            System.out.println(a%b);
+            System.out.println("");
+            System.out.println("*************");
+        
+        System.out.println("addition operation with strings and integers");
+        System.out.println("");
+            
+        String s="welcome to nucot";
+        String ss="bengaluru";
+    
+            System.out.println(a+s);
+            System.out.println(a+b+s);
+            System.out.println(a+b+s+ss);
+            System.out.println(s+ss+a+b);
+            System.out.println(s+ss+(a+b));
+            
+            System.out.println("");
+            System.out.println("*************");
+        
+                
+        System.out.println("addition operation with strings and Char");
+        System.out.println("");
+        
+        char c='a';
+        char cc='b';
+        
+            System.out.println(s+ss);
+            System.out.println(c+s);
+            System.out.println(c+s+ss);
+            System.out.println(c+cc+s+ss);
+            System.out.println(s+ss+c+cc);
+            System.out.println(s+ss+(c+cc));
+
+            System.out.println("");
+            System.out.println("*************");
+        
+        
+        System.out.println("addition operation with strings and short");
+        System.out.println("");
+
+        short sh=123;
+        short shh=456;
+        
+            System.out.println(sh+shh);
+            System.out.println(sh+s);
+            System.out.println(sh+s+ss);
+            System.out.println(sh+shh+s+ss);
+            System.out.println(s+ss+sh+shh);
+            System.out.println(s+ss+(sh+shh));
+
+            System.out.println("");
+            System.out.println("*************");
+
+        System.out.println("addition operation with strings and long");
+        System.out.println("");
+
+        long l=123456789l;
+        long ll=1234567890l;
+            
+            System.out.println(l+ll);
+            System.out.println(l+s);
+            System.out.println(l+s+ss);
+            System.out.println(l+ll+s+ss);
+            System.out.println(s+ss+l+ll);
+            System.out.println(s+ss+(l+ll));
+
+            System.out.println("");
+            System.out.println("*************");
+                
+        System.out.println("addition operation with strings and float");
+        System.out.println("");
+
+        float f=30.00f;
+        float ff=60.000f;
+                
+            System.out.println(f+ff);
+            System.out.println(f+s);
+            System.out.println(f+s+ss);
+            System.out.println(f+ff+s+ss);
+            System.out.println(s+ss+f+ff);
+            System.out.println(s+ss+(f+ff));
+            System.out.println("");
+            System.out.println("*************");
+    
+    
+        System.out.println("addition operation with strings and double");
+        System.out.println("");
+
+        double d=70.000000;
+        double dd=90.000000;
+                    
+            System.out.println(d+dd);
+            System.out.println(d+s);
+            System.out.println(d+s+ss);
+            System.out.println(d+dd+s+ss);
+            System.out.println(s+ss+d+dd);
+            System.out.println(s+ss+(d+dd));
+            System.out.println("");
+            System.out.println("*************");    
+    
+        
+    }
+
+}
