12b6b4abb1a40b9cea86cfbd636e4c576f83c379
prithamreddyt <prithamreddy100@gmail.com>
2024-02-28 08:53:48 +0530

    loops in Java

---
+++ Loops.java
@@ -0,0 +1,155 @@
+package Javasessions;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Types of loops");
+		System.out.println("");
+		System.out.println("while loop");
+		
+		//Syntax
+		/* while(condition)
+		*{
+		*
+		*}
+		*/
+		System.out.println("Program using while loop to get numbers less than 10");
+		System.out.println("");
+
+				int number=1;
+				while(number<=10)
+				{
+					System.out.println(number);
+					number++;
+				}
+				System.out.println("while loop ends when the number is greater than 10");
+		System.out.println("");
+		System.out.println("Program using while loop to get EVEN numbers less than 10");
+		System.out.println("");
+
+				int number2=2;
+				while(number2<=10)
+				{
+					System.out.println(number2);
+					number2=number2+2;
+				}
+				System.out.println("while loop ends when the number2 is greater than 10");
+		System.out.println("");
+		
+		System.out.println("Program using while loop to get ODD numbers less than 10");
+		System.out.println("");
+				int number3=1;
+				while(number3<=10)
+				{
+					System.out.println(number3);
+					number3=number3+2;
+				}
+				System.out.println("while loop ends when the number3 is greater than 10");
+		
+		System.out.println("");
+		System.out.println("");
+		
+		System.out.println("do while of loop");
+		
+		//Syntax
+		
+		/* do
+		 * {
+		 * 	body or statements
+		 * }while(condition);
+		 */
+	
+		System.out.println("Program using 'do while loop' to get numbers less than 10");
+		System.out.println("");
+
+				int number4=1;
+				do
+				{
+					System.out.println(number4);
+					number4++;
+				}while(number4<=10);
+				System.out.println("do while loop ends when the number4 is greater than 10");
+		System.out.println("");
+		
+		System.out.println("Program using 'do while loop' to get EVEN numbers less than 10");
+		System.out.println("");
+
+				int number5=2;
+				do
+				{
+					System.out.println(number5);
+					number5=number5+2;
+				}while(number5<=10);
+				System.out.println("do while loop ends when the number5 is greater than 10");
+		System.out.println("");
+		
+		System.out.println("Program using 'do while loop' to get ODD numbers less than 10");
+		System.out.println("");
+
+				int number6=1;
+				do
+				{
+					System.out.println(number6);
+					number6=number6+2;
+				}while(number6<=10);
+				System.out.println("do while loop ends when the number6 is greater than 10");
+		System.out.println("");
+		
+		
+		System.out.println("for loop");
+		
+		//Syntax
+		
+		/* for(variable_initialization;variable_condition;variable_updation)
+		*{
+		*	body or statement;
+		*}
+		*/
+		System.out.println("Program using 'for loop' to get numbers less than 10");
+		System.out.println("");
+
+				for(int number7=1;number7<=10;number7++)
+				{
+					System.out.println(number7);
+				}
+				System.out.println("for loop ends when the number7 is greater than 10");
+		System.out.println("");
+		
+		System.out.println("Program using 'for loop' to get EVEN numbers less than 10");
+		System.out.println("");
+
+				for(int number8=2;number8<=10;number8=number8+2)
+				{
+					System.out.println(number8);
+				}
+				System.out.println("for loop ends when the number8 is greater than 10");
+		System.out.println("");
+		
+		System.out.println("Program using 'for loop' to get ODD numbers less than 10");
+		System.out.println("");
+
+				for(int number9=1;number9<=10;number9=number9+2)
+				{
+					System.out.println(number9);
+				}
+				System.out.println("for loop ends when the number6 is greater than 10");
+		System.out.println("");
+
+		
+		//System.out.println("for each loop");
+		//Syntax
+		
+		/* while(condition)
+		*{
+		*
+		*}
+		*
+		*/
+		
+		
+
+	}
+
+}
